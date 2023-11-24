12b057d73e8c8f6ebc254a30f65e2e32d9f57ec3
Berat Demir <brtdmr2726@gmail.com>
2023-11-24 22:58:44 +0300

    24.11.23

---
+++ src/test/java/stepdefinitions/ExcelStepDefinitions.java
@@ -0,0 +1,11 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+
+public class ExcelStepDefinitions {
+
+    @Given("kullanici {string} bilgileri ile giris yapar")
+    public void kullanici_bilgileri_ile_giris_yapar(String string) {
+
+    }
+}
---
+++ src/test/resources/features/c06_excel_automation.feature
@@ -0,0 +1,4 @@
+@excel_automation
+Feature:
+  Scenario: TC01_musteri_giris_testi
+    Given kullanici "customer_info" bilgileri ile giris yapar
