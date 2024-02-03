0dbb76c4450d2ad3b76eaa4d0261c72f6cc4aaca
Berat Demir <brtdmr2726@gmail.com>
2024-02-03 12:40:35 +0300

    03.02.24

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -7,8 +7,8 @@ import utilities.Driver;
 
 public class HomePage {
 
-    public HomePage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    public HomePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
     @FindBy(xpath = "(//a[@role='button'])[1]")
