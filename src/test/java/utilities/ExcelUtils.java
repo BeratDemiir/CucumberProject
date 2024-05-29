6b19b3b2564f2a78883453fe67f0d962240fb768
Berat Demir <brtdmr2726@gmail.com>
2024-05-29 14:07:48 +0300

    29.05.2024

--- src/test/java/utilities/ExcelUtils.java
+++ src/test/java/utilities/ExcelUtils.java
@@ -73,7 +73,7 @@ public class ExcelUtils {
         }
     }
 
-    //============  getting all data into two dimentional array and returning the data  ===
+    //============  getting all data into two dimentional array and returning the data  ==========//
     public String[][] getDataArray() {
         String[][] data = new String[rowCount()][columnCount()];
         for (int i = 0; i < rowCount(); i++) {
